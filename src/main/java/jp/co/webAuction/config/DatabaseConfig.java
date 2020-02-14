3938974ed883715de3a8fdcdd5110d37ff77617a
8025kento <8025kento@gmail.com>
2020-02-14 21:01:07 +0900

    コミット

--- src/main/java/jp/co/webAuction/config/DatabaseConfig.java
+++ src/main/java/jp/co/webAuction/config/DatabaseConfig.java
@@ -2,23 +2,20 @@ package jp.co.webAuction.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DatabaseConfig {
 
-	@Value("postgres://gqhgjalxfmqfnj:b5b57fad8ecbe6208e92d25b219e10eb808cb86e4abffd5449a901d2832386cc@ec2-3-215-41-107.compute-1.amazonaws.com:5432/dabkol4sarbsqh")
-	private String dbUrl;
-
 	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "spring.datasource")
 	public DataSource dataSource() {
-		HikariConfig config = new HikariConfig();
-		config.setJdbcUrl(dbUrl);
-		return new HikariDataSource(config);
+		return DataSourceBuilder.create()
+				.build();
 	}
 }
