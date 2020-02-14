ef3e488802314f59b0e40d0f0eefbe050917de89
8025kento <8025kento@gmail.com>
2020-02-14 15:53:26 +0900

    コミット

--- src/main/java/jp/co/example/SpringHerokuTestApplication.java
+++ src/main/java/jp/co/example/SpringHerokuTestApplication.java
@@ -14,7 +14,7 @@ public class SpringHerokuTestApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-      return "Hello World! Tset eclipse B";
+      return "Hello World! Tset eclipse A";
     }
 
     public static void main(String[] args) {
