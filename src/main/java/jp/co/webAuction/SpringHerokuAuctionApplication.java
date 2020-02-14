1cd3a744191af12953e7d43f80d2301e966476d3
8025kento <8025kento@gmail.com>
2020-02-14 17:09:28 +0900

    コミット

--- src/main/java/jp/co/webAuction/SpringHerokuAuctionApplication.java
+++ src/main/java/jp/co/webAuction/SpringHerokuAuctionApplication.java
@@ -2,22 +2,20 @@ package jp.co.webAuction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
-@Controller
 @SpringBootApplication
-public class SpringHerokuTestApplication {
+public class SpringHerokuAuctionApplication {
 
-    @RequestMapping("/")
+	@RequestMapping("/")
     @ResponseBody
     String home() {
-      return "Hello World! Tset eclipse pom filter 12 ";
+      return "Hello World!";
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringHerokuTestApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHerokuAuctionApplication.class, args);
+	}
+
 }
