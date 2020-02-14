f8f48656711c72e86ab935992cd1a62399bc6dc3
8025kento <8025kento@gmail.com>
2020-02-14 18:35:13 +0900

    コミット

--- src/main/java/jp/co/webAuction/SpringHerokuAuctionApplication.java
+++ src/main/java/jp/co/webAuction/SpringHerokuAuctionApplication.java
@@ -2,19 +2,9 @@ package jp.co.webAuction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 public class SpringHerokuAuctionApplication {
-
-	@RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World! JSP";
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringHerokuAuctionApplication.class, args);
