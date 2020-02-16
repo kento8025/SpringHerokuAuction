797b7be494361147c2fa974d873a0a667d29f529
8025kento <8025kento@gmail.com>
2020-02-16 16:11:33 +0900

    コミット

--- src/main/java/jp/co/webAuction/DatabaseConfig.java
+++ src/main/java/jp/co/webAuction/DatabaseConfig.java
@@ -12,7 +12,7 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 public class DatabaseConfig {
 
-  @Value("${spring.datasource.url}")
+  @Value("postgres://nxfaxdtavfgalh:234962a1f8d6b2f4198a38709adec44357f4207b94786d840a9711e0295eee7e@ec2-50-17-178-87.compute-1.amazonaws.com:5432/dea9d804tl2d36")
   private String dbUrl;
 
   @Bean
